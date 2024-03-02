6fd871cbce2bada5c6cf58fd2f2c1ff76547a180
Barbecue23 <sunsanasupphong_j@silpakorn.com>
2024-03-02 21:49:37 +0700

    Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
-//import 'package:coffee_shop/pages/caet_page.dart';
-import 'package:coffee_shop/pages/test_page.dart';
+import 'package:coffee_shop/pages/caet_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Coffee Shop App',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        //scaffoldBackgroundColor: Color(0xFF0C0F14),
+        scaffoldBackgroundColor: Color(0xFF0C0F14),
       ),
-      home: const TestPage(),
+      home: const CartPage(),
     );
   }
 }
